1f9aa9dfcbd125c6e9cb0f35e9db716523c65202
Ahmed Aboalasaad <ahmed.aboalisaad@gmail.com>
2023-08-16 14:39:20 +0200

    preparation

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_string(char *s);
+int int_pow(int b, int p);
+int print_integer(int n);
+int print_binary(unsigned int n);
+
+#endif
